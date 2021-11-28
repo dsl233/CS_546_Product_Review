5f1695fbfdc2333c581c810eda660beb533fa229
dsl233 <44825481+dsl233@users.noreply.github.com>
2021-11-28 11:00:36 -0500

    Delete index.js

--- server/data/index.js
+++
@@ -1,9 +0,0 @@
-import usersData from "./users";
-import data_Products from "./products";
-import reviewsData from "./reviews";
-
-module.exports = {
-  users: usersData,
-  reviews:reviewsData,
-  products: data_Products
-};
